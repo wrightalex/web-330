df5fec283f33794781c4123e917623b959c7615f
wrightalex <wrightalex199@gmail.com>
2022-05-24 16:11:26 -0700

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,10 @@
+function setDefaultTheme()
+{
+    const theme = localStorage.getItem("mode") || "light-theme";
+    const iconMode = localStorage.getItem("iconMode") || "fa-toggle-off";
+    const iconText = localStorage.getItem("iconText") || "Light Mode";
+
+    document.body.classList.value = theme;
+    document.getElementById("icon-mode").classList.add(iconMode);
+    document.getElementById("icon-text").innerHTML = iconText;
+}
